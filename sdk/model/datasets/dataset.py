590138787ff027c5ea72a66dc4ffc038fc62c333
Jannes <jannes.magnusson@hhi.fraunhofer.de>
2024-04-03 15:30:32 +0200

    fix datasets

--- sdk/model/datasets/dataset.py
+++ sdk/model/datasets/dataset.py
@@ -5,8 +5,6 @@ import uuid
 @dataclass
 class Dataset:
     name: str # Unique in Feature (Reference for Dashboard Template)
-    type : str = None
-    id: uuid = None
 
     def __post_init__(self):
         self.id = uuid.uuid4()
--- sdk/model/geojson/property.py
+++ sdk/model/geojson/property.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import uuid
 from sdk.model.datasets.dataset import Dataset
 from typing import List
 
 @dataclass
 class GeoFeatureProperty:
-    roiID: uuid
     type: str
-    elevation: float # Elevation over 0 
-    datasets: List[Dataset]
+    elevation: float = None # Elevation over 0 
+    roiID: uuid = None
+    datasets: List[Dataset] = field(default_factory=list)
 
     def __post_init__(self):
         self.roiID = uuid.uuid4()
