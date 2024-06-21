3fe2f0b151b1feea1b275ca627320789f15f30ed
Jan Redlich <jan.redlich@bonnconsulting.group>
2024-06-21 12:10:07 +0200

    New Try: rabbitMQ Fix

--- sdk/helper/rabbitmq_legacy.py
+++ sdk/helper/rabbitmq_legacy.py
@@ -9,16 +9,15 @@ class RabbitMQHelper:
     A request/reply pattern is implemented.
     """
 
-    def __init__(self, endpoint: str, port: int, routing_key: str, username: str, password: str, path: str = "") -> None:
-        """
-        Connection setup RabbitMQ 
+    def __init__(self, endpoint:str, port:int ,routing_key:str, username:str, password:str, path:str ="") -> None:
 
-        :param endpoint: endpoint to RabbitMQ (without http/https)
+        """
+        connection setup RabbitMQ 
+
+        :param endpoint: endpoint to RabbitMQ ( withot http\https)
         :param port: port of the endpoint 
-        :param routing_key: the routing key for sending the message  
-        :param username: RabbitMQ username
-        :param password: RabbitMQ password
-        :param path: optional path for message storage
+        :param routing_key: the routing key for send the Message  
+
         """
         
         logging.basicConfig(format='%(asctime)s - %(message)s')
@@ -27,10 +26,11 @@ class RabbitMQHelper:
 
         try:
             self.credentials_intern = pika.PlainCredentials(username, password)
-            self.connection_parameters = pika.ConnectionParameters(endpoint, port, "/", self.credentials_intern)
-            self.connection = pika.BlockingConnection(self.connection_parameters)
+            self.connection_paramters = pika.ConnectionParameters(endpoint,port,"/",self.credentials_intern)
+            self.connection = pika.BlockingConnection(self.connection_paramters)
             self.channel = self.connection.channel()
-            self.channel.queue_declare(queue=routing_key, durable=True)
+            # self.channel.queue_declare(routing_key,durable=True)
+            self.channel.queue_bind(routing_key)
             self.logger.debug("Connection to RabbitMQ successful.")
         except Exception as exp:
             self.logger.error(f"Connection error to RabbitMQ. \n Message: {exp}")
@@ -40,23 +40,24 @@ class RabbitMQHelper:
         self.f = None
         self.args = None
         self.path = path
-        self.message = None 
+        self.messag = None 
 
     def check_and_create_path(self):
-        if not os.path.exists(self.path):
+        if(not os.path.exists(self.path)):
             os.makedirs(self.path)
 
-    def write_message(self, queue, message):
+    def write_message(self,queue, messag):
         """
-        Sends/writes a message on the queue  
+        sends/write an message on the queue  
 
         :param queue: name of the queue
-        :param message: message to be sent
+        :param messag: message to be sent
         """
-        self.channel.basic_publish(exchange='', routing_key=queue, body=message)
+        # self.channel.queue_declare(queue)
+        self.channel.basic_publish(exchange='', routing_key = queue, body=messag)
 
     def get_message(self, queue):
-        method_frame, header_frame, body = self.channel.basic_get(queue)
+        method_frame, header_frame, body = self.channel.basic_get('test')
         if method_frame:
             print(method_frame, header_frame, body)
             self.channel.basic_ack(method_frame.delivery_tag)
@@ -70,11 +71,13 @@ class RabbitMQHelper:
         channel.basic_ack(delivery_tag=method_frame.delivery_tag)
     
     def listen(self, queue, method=None):
+        self.channel = self.connection.channel()
         if method is None:
             method = self.on_message
-        self.channel.basic_consume(queue=queue, on_message_callback=method, auto_ack=False)
+        self.channel.basic_consume(queue, method)
         try:
             self.channel.start_consuming()
         except KeyboardInterrupt:
             self.channel.stop_consuming()
         self.connection.close()
+
