fe0c6d850c7320a26a92007f51bf9f8104ac42c4
Jan Christian Redlich <jan.redlich@bonnconsulting.group>
2024-06-25 21:16:44 +0200

    only parse relevant Messages (start messages)

--- sdk/model/action/action.py
+++ sdk/model/action/action.py
@@ -16,7 +16,11 @@ class Action():
     @classmethod
     def from_json(cls,json_string):
         json_dict = json.loads(json_string)
-        action_data = ActionData(S3Bucket(json_dict['data']['bucket']['endpoint'], json_dict['data']['bucket']['port'], json_dict['data']['bucket']['name'], json_dict['data']['bucket']['accessId'], json_dict['data']['bucket']['accessToken']), json_dict['data']['inputData'], json_dict['data']['outputData'])
+        if json_dict['pattern'] == "start":
+            action_data = ActionData(S3Bucket(json_dict['data']['bucket']['endpoint'], json_dict['data']['bucket']['port'], json_dict['data']['bucket']['name'], json_dict['data']['bucket']['accessId'], json_dict['data']['bucket']['accessToken']), json_dict['data']['inputData'], json_dict['data']['outputData'])
+        else:
+            action_data = None
+            
         action = Action(json_dict['pattern'], action_data)
         return action
 
